0bf818b333bebc485774731415c433f215bf1f57
Vivek <86357069+truly-vivek@users.noreply.github.com>
2024-02-26 01:37:24 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -201,6 +201,7 @@ srtop.reveal('.home .image', { delay: 400 });
 srtop.reveal('.home .linkedin', { interval: 600 });
 srtop.reveal('.home .github', { interval: 800 });
 srtop.reveal('.home .twitter', { interval: 1000 });
+srtop.reveal('.home .mail', { interval: 600 });
 srtop.reveal('.home .telegram', { interval: 600 });
 srtop.reveal('.home .instagram', { interval: 600 });
 srtop.reveal('.home .dev', { interval: 600 });
